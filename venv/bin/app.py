eceb769a654d0720a30dd99fa11efd79316a1538
Mahati Gollamudi <github email address>
2016-11-17 18:02:02 -0500

    added virtual env and rest package extension and started outline

---
+++ venv/bin/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+## each game is a resource, a thing that our api can return
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Game(Resource):
+    def get(self, gamename):
+        return { 'game' : gamename}
+        
